fe0f20110ad292b59e1e1ac8de3091920465977e
Ícaro Lorran Lopes Costa <icarosadero@gmail.com>
2024-12-06 18:43:47 +0100

    checkpoint

---
+++ fuhmann_data_etl/main.py
@@ -0,0 +1,29 @@
+#%%
+import dask.config
+import pandas as pd
+from dask.distributed import Client
+import dask.dataframe as dd
+from glob import glob
+import os
+from dotenv import load_dotenv
+from tqdm import tqdm
+import logging
+
+load_dotenv("../.env")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+
+INGEST_PATH = os.environ["INGEST_PATH"]
+TARGET_PATH = os.environ["TARGET_PATH"]
+DASK_SCHEDULER_ADDRESS = os.environ["DASK_SCHEDULER_ADDRESS"]
+
+client = Client(DASK_SCHEDULER_ADDRESS)
+# %%
