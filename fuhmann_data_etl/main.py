e24aa8115bef6d08a6ab2a72b7c3951962f71048
Ícaro Lorran Lopes Costa <icarosadero@gmail.com>
2024-12-09 17:02:59 +0100

    Include ingestion

---
+++ fuhmann_data_etl/main.py
@@ -0,0 +1,20 @@
+#%%
+import os
+from dotenv import load_dotenv
+import logging
+
+load_dotenv("../.env")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.propagate = False
+
+INGEST_PATH = os.environ["INGEST_PATH"]
+TARGET_PATH = os.environ["TARGET_PATH"]
+DASK_SCHEDULER_ADDRESS = os.environ["DASK_SCHEDULER_ADDRESS"]
